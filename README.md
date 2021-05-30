deafab23e21c51f2f304ede8295f51ecec01a45d
minidonut <nycom13@gmail.com>
2021-05-30 23:00:16 +0900

    Add OSX

---
+++ README.md
@@ -0,0 +1,18 @@
+# ScreenResolutions
+
+Get screen resolutions
+
+```ts
+import { resolutions } from 'screen-resolutions'
+
+(async () => {
+  console.log(await resolutions());
+
+  // [
+  //   { w: 3072, h: 1920, monitor: 'Retina' },
+  //   { w: 2560, h: 1080, monitor: '(UW-UXGA - Ultra Wide - Ultra Extended Graphics Array)' }
+  // ]
+
+});
+
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("hello screen-resolutions");
+import { promisify } from 'util';
+import { exec as _exec } from 'child_process';
+
+const exec = promisify(_exec);
+
+export interface Resolution {
+  w: number;
+  h: number;
+  monitor: string;
+}
+
+const resolverMap: Partial<Record<typeof process.platform, () => Promise<Resolution[]>>> = {
+  darwin: async () => {
+    const { stdout } = await exec('system_profiler SPDisplaysDataType');
+    const regex = new RegExp(/Resolution: (\d+?) x (\d+?) (.*)/);
+    const resolutions: Resolution[] = [];
+    for (const line of stdout.split('\n')) {
+      const parsed = regex.exec(line);
+      if (parsed) {
+        resolutions.push({
+          w: parseInt(parsed[1]),
+          h: parseInt(parsed[2]),
+          monitor: parsed[3],
+        });
+      }
+    }
+
+    return resolutions;
+  },
+};
+
+export async function resolutions(): Promise<Resolution[]> {
+  const resolve = resolverMap[process.platform];
+  if (resolve === undefined) {
+    throw new Error(`Platform not supported: ${process.platform}`);
+  }
+  return resolve();
+}
+
+export default resolutions;
